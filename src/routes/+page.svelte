4f3672a29be1777f912f293f8463632fca39b378
Philipp <liebphil14@gmail.com>
2026-04-17 19:57:16 +0200

    ...

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,158 +1,54 @@
 <script>
-  import { invoke } from "@tauri-apps/api/core";
+    import { goto } from "$app/navigation";
+    import { show } from "@tauri-apps/api/app";
+    import { invoke } from "@tauri-apps/api/core";
+    import { on } from "svelte/events";
+    import 'src/style.css';
 
-  let name = $state("");
-  let greetMsg = $state("");
+  let showPopup = false;
 
-  async function greet(event) {
-    event.preventDefault();
-    // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-    greetMsg = await invoke("greet", { name });
+  function openPopup() {
+    showPopup = true;
+  }
+
+  function closePopup() {
+    showPopup = false;
   }
 </script>
 
 <main class="container">
-  <h1>Welcome to Tauri + Svelte</h1>
+  <section class="hero">
+    <div class="title-group">
+      <img src="src//img/bonfire_icon_4k.png" alt="Bonfire logo" class="logo" />
+      <h1 id="titletext">Bonfire</h1>
+    </div>
 
-  <div class="row">
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo vite" alt="Vite Logo" />
-    </a>
-    <a href="https://tauri.app" target="_blank">
-      <img src="/tauri.svg" class="logo tauri" alt="Tauri Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank">
-      <img src="/svelte.svg" class="logo svelte-kit" alt="SvelteKit Logo" />
-    </a>
+    <nav class="bar">
+      <a href="/register">Register</a>
+      <a href="/login">Login</a>
+    </nav>
+
+    <p>Welcome to Bonfire! Bonfire is a modern Discord-like platform designed for real-time conversation, shared spaces, and meaningful connections. Create servers, join communities, chat with friends, and stay connected through a fast, simple, and welcoming experience.</p>
+
+    <button id="goto-btn" onclick={() => openPopup()}>Get started</button>
+   {#if showPopup}
+  <div class="popup-overlay" onclick={closePopup}>
+    <div
+      class="popup"
+      onclick={(event) => {
+        event.stopPropagation();
+      }}
+    >
+      <h2>Welcome to Bonfire</h2>
+      <p>Start by creating an account or logging in.</p>
+      <button id="button-main" onclick={() => goto("/register")}>Register</button>
+      <button id="button-main" onclick={() => goto("/login")}>Login</button>
+      <div class="close-btn">
+        <button id="close-btn" onclick={closePopup}>Close</button>
+      </div>
+    </div>
   </div>
-  <p>Click on the Tauri, Vite, and SvelteKit logos to learn more.</p>
-
-  <a href="/register">Zur Register-Seite</a>
-
-  <form class="row" onsubmit={greet}>
-    <input id="greet-input" placeholder="Enter a name..." bind:value={name} />
-    <button type="submit">Greet</button>
-  </form>
-  <p>{greetMsg}</p>
+{/if}
+  </section>
 </main>
 
-<style>
-.logo.vite:hover {
-  filter: drop-shadow(0 0 2em #747bff);
-}
-
-.logo.svelte-kit:hover {
-  filter: drop-shadow(0 0 2em #ff3e00);
-}
-
-:root {
-  font-family: Inter, Avenir, Helvetica, Arial, sans-serif;
-  font-size: 16px;
-  line-height: 24px;
-  font-weight: 400;
-
-  color: #0f0f0f;
-  background-color: #f6f6f6;
-
-  font-synthesis: none;
-  text-rendering: optimizeLegibility;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  -webkit-text-size-adjust: 100%;
-}
-
-.container {
-  margin: 0;
-  padding-top: 10vh;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  text-align: center;
-}
-
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: 0.75s;
-}
-
-.logo.tauri:hover {
-  filter: drop-shadow(0 0 2em #24c8db);
-}
-
-.row {
-  display: flex;
-  justify-content: center;
-}
-
-a {
-  font-weight: 500;
-  color: #646cff;
-  text-decoration: inherit;
-}
-
-a:hover {
-  color: #535bf2;
-}
-
-h1 {
-  text-align: center;
-}
-
-input,
-button {
-  border-radius: 8px;
-  border: 1px solid transparent;
-  padding: 0.6em 1.2em;
-  font-size: 1em;
-  font-weight: 500;
-  font-family: inherit;
-  color: #0f0f0f;
-  background-color: #ffffff;
-  transition: border-color 0.25s;
-  box-shadow: 0 2px 2px rgba(0, 0, 0, 0.2);
-}
-
-button {
-  cursor: pointer;
-}
-
-button:hover {
-  border-color: #396cd8;
-}
-button:active {
-  border-color: #396cd8;
-  background-color: #e8e8e8;
-}
-
-input,
-button {
-  outline: none;
-}
-
-#greet-input {
-  margin-right: 5px;
-}
-
-@media (prefers-color-scheme: dark) {
-  :root {
-    color: #f6f6f6;
-    background-color: #2f2f2f;
-  }
-
-  a:hover {
-    color: #24c8db;
-  }
-
-  input,
-  button {
-    color: #ffffff;
-    background-color: #0f0f0f98;
-  }
-  button:active {
-    background-color: #0f0f0f69;
-  }
-}
-
-</style>
--- src/routes/register/+page.svelte
+++ src/routes/register/+page.svelte
@@ -1,2 +1,15 @@
+<script>
+    import { goto } from "$app/navigation";
+    import { show } from "@tauri-apps/api/app";
+    import { invoke } from "@tauri-apps/api/core";
+    import { on } from "svelte/events";
+    import 'src/style.css';
+</script>
+
+<main class="container">
+
 <h1>Register</h1>
 
+
+<button id="button-return" onclick={() => goto("/")}>Go back</button>
+</main>
